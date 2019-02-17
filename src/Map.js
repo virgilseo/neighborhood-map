f564eb412801b647f47c5219fd3452d0c30b72de
Virgil-Avram <avraam.virgil@yahoo.com>
2019-02-17 18:48:59 +0100

    Display the default five markers on the page

--- src/Map.js
+++ src/Map.js
@@ -1,15 +1,40 @@
 import React, {Component} from 'react'
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
+//Implementation for loadind the map taken from https://www.npmjs.com/package/google-maps-react
+
+const places = [
+  { lat: 51.221569, lng: 6.785354 },
+  { lat: 51.218579, lng: 6.784461 },
+  { lat: 51.219820, lng: 6.766711 },
+  { lat: 51.223832, lng: 6.771095 },
+  { lat: 51.227542, lng: 6.775780 }
+]
 export class MapContainer extends Component {
+
+//Set Initail state
+  state = {
+    places:[]
+  }
+
+  componentDidMount() {
+    this.setState({ places:places })
+  }
+
   render() {
+    console.log(places)
     return (
       <Map
         google={this.props.google}
         zoom={14}
         initialCenter={{ lat: 51.22172, lng: 6.77616 }}
       >
-        <Marker />
+        {this.state.places.map((place) => (
+          <Marker
+            position={{ lat:place.lat, lng:place.lng }}
+            key={place.lat}
+          />
+         ))}
       </Map>
     );
   }
