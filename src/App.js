6161ecaf5b8d964ee0a7fe2511a098fb6e02137f
Virgil-Avram <avraam.virgil@yahoo.com>
2019-02-19 08:41:16 +0100

    Refactor code in main app component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,52 @@ import React, { Component } from 'react';
 import './App.css';
 import MapContainer from './Map'
 
+//Store all the information about the locations in a array of objects
+const places = [
+  { id: 'h6N9N', name: 'KIT - Kunst im Tunnel', address: 'Mannesmannufer 1b, 40213 Düsseldorf', lat: 51.220050, lng: 6.767270 },
+  { id: 'iH6M9', name: 'City Museum', address: 'Berger Allee 2, 40213 Düsseldorf', lat: 51.218579, lng: 6.769980 },
+  { id: 'h78Hg', name:'TeamEscape Düsseldorf', address: 'Alexanderstraße 31, 40210 Düsseldorf',  lat: 51.220050, lng: 6.783620 },
+  { id: 'k90Ms', name: 'Stahlhof', address: 'Bastionstraße 39, 40213 Düsseldorf', lat: 51.221380, lng: 6.776260 },
+  { id: 'kl12B', name:'Schadow-Arkaden', address:'Schadowstraße 11, 40212 Düsseldorf', lat: 51.225510, lng: 6.780310 }
+]
+
 class App extends Component {
+
+//Set Initail state
+  state = {
+    places:[],
+    showingInfoWindow: false,
+    activeMarker: {},
+    locationName:[],
+    locationAddress:[]
+  }
+
+  componentDidMount() {
+    this.setState({ places:places })
+  }
+
+  //Handle marker click and set visibility for the info window
+
+  onMarkerClick = (props, marker, e) =>
+    this.setState({
+      activeMarker: marker,
+      showingInfoWindow: true,
+      locationName: marker.name,
+      locationAddress: marker.address
+    });
+
   render() {
     return (
       <div className="App">
-        <MapContainer />
+        <MapContainer
+          google={this.props.google}
+          places={this.state.places}
+          showingInfoWindow={this.state.showingInfoWindow}
+          activeMarker={this.state.activeMarker}
+          locationName={this.state.locationName}
+          locationAddress={this.state.locationAddress}
+          onMarkerClick={this.onMarkerClick}
+        />
       </div>
     );
   }
--- src/Map.js
+++ src/Map.js
@@ -3,49 +3,19 @@ import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
 //Implementation for loadind the map taken from https://www.npmjs.com/package/google-maps-react
 
-const places = [
-  { id: 'h6N9N', name: 'KIT - Kunst im Tunnel', address: 'Mannesmannufer 1b, 40213 Düsseldorf', lat: 51.220050, lng: 6.767270 },
-  { id: 'iH6M9', name: 'City Museum', address: 'Berger Allee 2, 40213 Düsseldorf', lat: 51.218579, lng: 6.769980 },
-  { id: 'h78Hg', name:'TeamEscape Düsseldorf', address: 'Alexanderstraße 31, 40210 Düsseldorf',  lat: 51.220050, lng: 6.783620 },
-  { id: 'k90Ms', name: 'Stahlhof', address: 'Bastionstraße 39, 40213 Düsseldorf', lat: 51.221380, lng: 6.776260 },
-  { id: 'kl12B', name:'Schadow-Arkaden', address:'Schadowstraße 11, 40212 Düsseldorf', lat: 51.225510, lng: 6.780310 }
-]
 export class MapContainer extends Component {
 
-//Set Initail state
-  state = {
-    places:[],
-    showingInfoWindow: false,
-    activeMarker: {},
-    locationName:[],
-    locationAddress:[]
-  }
-
-  componentDidMount() {
-    this.setState({ places:places })
-  }
-
-  //Handle marker click and set visibility for the info window
-
-  onMarkerClick = (props, marker, e) =>
-    this.setState({
-      activeMarker: marker,
-      showingInfoWindow: true,
-      locationName: marker.name,
-      locationAddress: marker.address
-    });
-
   render() {
-    console.log(places)
+
     return (
       <Map
         google={this.props.google}
         zoom={14}
         initialCenter={{ lat: 51.22172, lng: 6.77616 }}
       >
-        {this.state.places.map((place) => (
+        {this.props.places.map((place) => (
           <Marker
-            onClick={this.onMarkerClick}
+            onClick={this.props.onMarkerClick}
             position={{ lat:place.lat, lng:place.lng }}
             key={place.id}
             name={place.name}
@@ -53,11 +23,11 @@ export class MapContainer extends Component {
           />
          ))}
            <InfoWindow
-              marker={this.state.activeMarker}
-              visible={this.state.showingInfoWindow}>
+              marker={this.props.activeMarker}
+              visible={this.props.showingInfoWindow}>
               <div>
-                <h1>{this.state.locationName}</h1>
-                <p>Address: {this.state.locationAddress}</p>
+                <h1>{this.props.locationName}</h1>
+                <p>Address: {this.props.locationAddress}</p>
               </div>
            </InfoWindow>
       </Map>
