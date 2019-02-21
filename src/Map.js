72bc5cde5f44cd33dc20fb825f5cb22b1f7f6a5b
Virgil-Avram <avraam.virgil@yahoo.com>
2019-02-21 18:24:36 +0100

    Display error message to the user if google maps api is not available

--- src/Map.js
+++ src/Map.js
@@ -5,45 +5,44 @@ import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
 export class MapContainer extends Component {
 
-
   render() {
- console.log(this.props.hits)
+
     return (
       <div className='map-container'>
-      <Map
-        google={this.props.google}
-        zoom={14}
-        initialCenter={{ lat: 51.22172, lng: 6.77616 }}
-      >
-        {this.props.places.map((place) => (
-          <Marker
-            onClick={this.props.onMarkerClick}
-            position={{ lat:place.lat, lng:place.lng }}
-            key={place.id}
-            name={place.name}
-            address={place.address}
-            animation={this.props.activeMarker ? (place.name === this.props.activeMarker.name ? this.props.google.maps.Animation.BOUNCE :'0') : '0'}
-            currentMarker={place}
-            id={place.id}
-          />
-         ))}
-           <InfoWindow
-              position={{lat: parseFloat(this.props.markerLat), lng: parseFloat(this.props.markerLng) }}
-              visible={this.props.showingInfoWindow}>
-              <div>
-              {this.props.hits !== [] && (
+        <Map
+          google={this.props.google}
+          zoom={14}
+          initialCenter={{ lat: 51.22172, lng: 6.77616 }}
+        >
+          {this.props.places.map((place) => (
+            <Marker
+              onClick={this.props.onMarkerClick}
+              position={{ lat:place.lat, lng:place.lng }}
+              key={place.id}
+              name={place.name}
+              address={place.address}
+              animation={this.props.activeMarker ? (place.name === this.props.activeMarker.name ? this.props.google.maps.Animation.BOUNCE :'0') : '0'}
+              currentMarker={place}
+              id={place.id}
+            />
+           ))}
+             <InfoWindow
+                position={{lat: parseFloat(this.props.markerLat), lng: parseFloat(this.props.markerLng) }}
+                visible={this.props.showingInfoWindow}>
                 <div>
-                  <h1>{this.props.hits.name}</h1>
-                  <p>{this.props.hits.timeZone}</p>
-                  <p>{this.props.hits.description }</p>
+                {this.props.hits !== [] && (
+                  <div>
+                    <h1>{this.props.hits.name}</h1>
+                    <p>{this.props.hits.timeZone}</p>
+                    <p>{this.props.hits.description }</p>
+                  </div>
+                )}
+                {this.props.error === 'error' && (
+                  <p>Something went wrong</p>
+                )}
                 </div>
-              )}
-              {this.props.error === 'error' && (
-                <p>Something went wrong</p>
-              )}
-              </div>
-           </InfoWindow>
-      </Map>
+             </InfoWindow>
+        </Map>
       </div>
     );
   }
