7f787498615a6e0f23fcb7cb94a29095f85f5431
Virgil-Avram <avraam.virgil@yahoo.com>
2019-06-22 15:29:04 +0200

    Add menu icon

--- src/Header.js
+++ src/Header.js
@@ -7,6 +7,7 @@ class Header extends Component {
     return(
       <div className='header-container'>
         <header className='header'>
+          <i className='material-icons menu-icon' role='button' name='Toggle siderbar' >menu</i>
           <h1 className='title'>Düsseldorf Places</h1>
         </header>
       </div>
