dac328f0e892e6638b750c19cc704fe50759e581
Virgil-Avram <avraam.virgil@yahoo.com>
2019-07-14 11:09:39 +0200

    Fix bug with info window position

--- src/Map.js
+++ src/Map.js
@@ -28,7 +28,7 @@ export class MapContainer extends Component {
            ))}
              <InfoWindow
                 onClose={this.props.hideInfoWindow}
-                position={{lat: parseFloat(this.props.markerLat), lng: parseFloat(this.props.markerLng)}}
+                position={{lat: parseFloat(this.props.markerLat) + 0.006, lng: parseFloat(this.props.markerLng)}}
                 visible={this.props.showingInfoWindow}>
                 <div className='info-window'>
                 {this.props.isLoading === true && (
