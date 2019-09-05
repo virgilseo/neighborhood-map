fbe60b48891ead09f059c580a6b11746f9bc8e62
Virgil-Avram <avraam.virgil@yahoo.com>
2019-09-05 15:52:53 +0200

    Add new locations

--- src/Locations.js
+++ src/Locations.js
@@ -15,7 +15,20 @@ const Locations = [
   { id: '4b5ed93ef964a520d79a29e3', name: 'Schumacher Stammhaus', type: 'brewery', lat: 51.221498311147, lng: 6.785565174084197 },
   { id: '4b6d861ff964a5202d7a2ce3', name: 'Brauhaus Joh. Albrecht', type: 'brewery', lat: 51.24036708537516, lng: 6.751411578633284 },
   { id: '4bdc1b9f63c5c9b6ec6a2a68', name: 'Museum Kunstpalast', type: 'museum', lat: 51.23492906744998, lng: 6.773236574142464 },
-  { id: '4b6e9e41f964a5202ac42ce3', name: 'K21 Ständehaus', type: 'museum', lat: 51.21664355455374, lng: 6.773998887902013 }
+  { id: '4b6e9e41f964a5202ac42ce3', name: 'K21 Ständehaus', type: 'museum', lat: 51.21664355455374, lng: 6.773998887902013 },
+  { id: '4df5f772d4c01ff6b2f43229', name: 'K20 Kunstsammlung NRW', type: 'museum', lat: 51.2283882217505, lng: 6.776057557999587 },
+  { id: '4be4150c477d9c745784e62d', name: 'Schifffahrt-Museum im Schlossturm', type: 'museum', lat: 51.22739946911818, lng: 6.770961994333302 },
+  { id: '4f8271d3e4b02f4581db9a07', name: 'NRW-Forum Düsseldorf', type: 'museum', lat: 51.233297620424025, lng: 6.772193315604253 },
+  { id: '4be59a4cbcef2d7f080004e5', name: 'Hetjens-Museum', type: 'museum', lat: 51.22378371335815, lng: 6.7710201389920135 },
+  { id: '515080378aca1877ebad8170', name: 'Sumi.', type: 'restaurant', lat: 51.231848045562096, lng: 6.79256622088267 },
+  { id: '4b5b2f0df964a52061e928e3', name: 'Roberts Bistro', type: 'restaurant', lat: 51.21540073559408, lng: 6.757224206816219 },
+  { id: '522f64fa498e5f18ab365a35', name: 'Principale Pizzabar', type: 'restaurant', lat: 51.22870397043688, lng: 6.801720427658401 },
+  { id: '50e4225be4b0a94f756be407', name: 'ĂN BÁNH MÌ', type: 'restaurant', lat: 51.229796719516976, lng: 6.807178463018556 },
+  { id: '4b4e2db3f964a5203ae426e3', name: 'Brasserie Hülsmann', type: 'restaurant', lat: 51.23151554570723, lng: 6.747708163817817 },
+  { id: '4c2cd74fae6820a1d4f91943', name: 'Tonhallen-Terrasse', type: 'brewery', lat: 51.23208601166795, lng: 6.772100336619741 },
+  { id: '4d0a5cff5edd54815c3e059b', name: 'Pizzeria Romantica', type: 'restaurant', lat: 51.23375784654132, lng: 6.792516021404531 },
+  { id: '4bab9683f964a520a7b63ae3', name: 'The Classic Western Steakhouse', type: 'restaurant', lat: 51.237144, lng: 6.793405 },
+  { id: '56f433f2498e3c2fc23de310', name: 'Bibimcup', type: 'restaurant', lat: 51.222627783532054, lng: 6.787039529351158 }
 ]
 
 export default Locations
